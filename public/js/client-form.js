bc9064a0b3bcd11e3229594cf0a54013b7d20c2e
Samad <samad.damas@hotmail.com>
2023-04-04 23:33:02 -0300

    Add client info validation

--- public/js/client-form.js
+++ public/js/client-form.js
@@ -1,20 +1,23 @@
 import Client from "./Client.mjs";
 import * as clientManager from "./ClientManager.mjs";
 
-const saveClient = document.querySelector("#save-client");
+const clientForm = document.querySelector("#client-form");
+const actionMessage = document.querySelector("#client-form #action-message")
 
 // Save client action
-saveClient.addEventListener("click", () => {
+clientForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   const clientInfo = clientManager.getNewClientInfo();
   const clientId = clientInfo.id;
   if (localStorage.getItem(clientId)) {
     clientManager.updateClient(clientInfo);
-    console.log("Client update" + clientId)
+    actionMessage.textContent = "Client updated!"
   } else {
     const client = new Client(clientInfo)
     clientManager.addClient(client);
+    actionMessage.textContent = "Client saved!"
   }
-  
 
   // Render client list with new client
   const clientListElement = document.querySelector("#client-list tbody");
